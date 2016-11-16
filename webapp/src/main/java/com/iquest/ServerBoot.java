b33822b58cebe0a12dcf985c190019bd41ffb59a
Stefan Pamparau <stefan.pamparau@iquestgroup.com>
2016-11-16 14:42:57 +0200

    Removed commented annotation.

--- webapp/src/main/java/com/iquest/ServerBoot.java
+++ webapp/src/main/java/com/iquest/ServerBoot.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "iquest")
-//@EntityScan(basePackages = "model")
 public class ServerBoot {
 
     public static void main(String[] args) {
